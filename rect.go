8e55fe72b6f673ce21c3b31e39cdf0fd3753a4bc
amendgit <shijian0912@163.com>
2016-12-25 17:40:28 +0800

    rename Rect receiver from r to rect.

--- rect.go
+++ rect.go
@@ -32,62 +32,62 @@ func MakeRectEmpty() Rect {
 }
 
 // Return te left edge of the rect.
-func (r Rect) L() Scalar {
-	return r.Left
+func (rect Rect) L() Scalar {
+	return rect.Left
 }
 
-func (r Rect) X() Scalar {
-	return r.Left
+func (rect Rect) X() Scalar {
+	return rect.Left
 }
 
 // Return the top edge of the rect.
-func (r Rect) T() Scalar {
-	return r.Top
+func (rect Rect) T() Scalar {
+	return rect.Top
 }
 
-func (r Rect) Y() Scalar {
-	return r.Top
+func (rect Rect) Y() Scalar {
+	return rect.Top
 }
 
 // Return the rectangle's width. This does not check for a valid rect
 // (i.e. left <= right) so the result may be negative.
-func (r Rect) W() Scalar {
-	return r.Width
+func (rect Rect) W() Scalar {
+	return rect.Width
 }
 
 // Returns the rectangle's height. This does not check for a vaild rect
 // (i.e. top <= bottom) so the result may be negative.
-func (r Rect) H() Scalar {
-	return r.Height
+func (rect Rect) H() Scalar {
+	return rect.Height
 }
 
 // Returns the rectangle's right edge.
-func (r Rect) R() Scalar {
-	return r.Left + r.Width
+func (rect Rect) R() Scalar {
+	return rect.Left + rect.Width
 }
 
 // Returns the rectangle's bottom edge.
-func (r Rect) B() Scalar {
-	return r.Top + r.Height
+func (rect Rect) B() Scalar {
+	return rect.Top + rect.Height
 }
 
 // Returns the rectangle's center x.
-func (r Rect) CenterX() Scalar {
-	return r.Left + r.Width*0.5
+func (rect Rect) CenterX() Scalar {
+	return rect.Left + rect.Width*0.5
 }
 
 // Returns the rectangle's center Y.
-func (r Rect) CenterY() Scalar {
-	return r.Top + r.Height*0.5
+func (rect Rect) CenterY() Scalar {
+	return rect.Top + rect.Height*0.5
 }
 
 // Return true if the rectangle's width or height are <= 0
-func (r Rect) IsEmpty() bool {
-	return r.Left <= 0 || r.Height <= 0
+func (rect Rect) IsEmpty() bool {
+	return rect.Left <= 0 || rect.Height <= 0
 }
 
-func (r Rect) SetEmpty() {
-	r.SetXYWH(0, 0, 0, 0)
+func (rect Rect) SetEmpty() {
+	rect.SetXYWH(0, 0, 0, 0)
 }
 
 // Return true if the two rectangles have same position and size.
@@ -96,23 +96,23 @@ func (a Rect) Equal(b Rect) bool {
 }
 
 // Set the rectangle's edges with (x, y, w, h)
-func (r *Rect) SetXYWH(x, y, width, height Scalar) {
-	r.Left, r.Top, r.Width, r.Height = x, y, width, height
+func (rect *Rect) SetXYWH(x, y, width, height Scalar) {
+	rect.Left, rect.Top, rect.Width, rect.Height = x, y, width, height
 }
 
 // Set the rectangle's edges with (left, top, right, bottom)
-func (r *Rect) SetLTRB(left, top, right, bottom Scalar) {
-	r.Left, r.Top, r.Width, r.Height = left, top, right-left, bottom-top
+func (rect *Rect) SetLTRB(left, top, right, bottom Scalar) {
+	rect.Left, rect.Top, rect.Width, rect.Height = left, top, right-left, bottom-top
 }
 
-func (r *Rect) SetLTRBPoint(lt, rb Point) {
-	r.Left = ScalarMin(lt.X, rb.X)
-	r.Top = ScalarMin(lt.Y, rb.Y)
-	r.Width = ScalarAbs(rb.X - lt.X)
-	r.Height = ScalarAbs(rb.Y - lt.Y)
+func (rect *Rect) SetLTRBPoint(lt, rb Point) {
+	rect.Left = ScalarMin(lt.X, rb.X)
+	rect.Top = ScalarMin(lt.Y, rb.Y)
+	rect.Width = ScalarAbs(rb.X - lt.X)
+	rect.Height = ScalarAbs(rb.Y - lt.Y)
 }
 
-func (r *Rect) IntersectXYWH(x, y, w, h Scalar) bool {
+func (rect *Rect) IntersectXYWH(x, y, w, h Scalar) bool {
 	toimpl()
 	return false
 }
@@ -124,6 +124,6 @@ func (rect *Rect) InsetLTRB(left, top, right, bottom Scalar) {
 	rect.Height = rect.Height - top + bottom
 }
 
-func (r Rect) ToGoRect() image.Rectangle {
-	return image.Rect(int(r.Left), int(r.Top), int(r.Width), int(r.Height))
+func (rect Rect) ToGoRect() image.Rectangle {
+	return image.Rect(int(rect.Left), int(rect.Top), int(rect.Width), int(rect.Height))
 }
