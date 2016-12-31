4f0958b07c63af5351c00577154c810edffc559e
amendgit <shijian0912@163.com>
2016-12-31 18:50:56 +0800

    add paint toimpl methods.

--- paint.go
+++ paint.go
@@ -1,7 +1,8 @@
 package ggk
 
-// Paint holds the style and color information about how to draw geometries, text
-// and bitmaps.
+/** Paint
+holds the style and color information about how to draw geometries, text
+and bitmaps. */
 type Paint struct {
 	flags       uint16
 	hinting     uint8
@@ -22,21 +23,54 @@ func NewPaint() *Paint {
 	return paint
 }
 
-// PaintHinting specifies the level of hinting to be performed. These names are
-// taken from the Gnome/Cairo names for the same. They are translated into
-// Freetype concepts the same as in cairo-ft-font.c:
-// KPaintHintingNo     -> FT_LOAD_NO_HINTING
-// KPaintHintingSlight -> FT_LOAD_TARGET_LIGHT
-// KPaintHintingNormal -> <default, no option>
-// KPaintHintingFull   -> <same as KPaintHintingNormal, unelss we are rendering
-//                         subpixel glyphs, in which case TARGET_LCD or
-//                         TARGET_LCD_V is used>
+func NewPaint_Clone(otr *Paint) *Paint {
+	toimpl()
+	return nil
+}
+
+/** Equal may give false negatives: two paints that draw equivalently
+may return false.  It will never give false positives: two paints that
+are not equivalent always return false. */
+func (paint *Paint) Equal(otr *Paint) bool {
+	toimpl()
+	return false
+}
+
+/** Hash is a shallow hash, with the same limitations as operator==.
+If operator== returns true for two paints, getHash() returns the same value for each. */
+func (paint *Paint) Hash() uint32 {
+	toimpl()
+	return 0
+}
+
+func (paint *Paint) Flatten(buffer *WriteBuffer) {
+	toimpl()
+}
+
+func (paint *Paint) Unflatten(buffer *ReadBuffer) {
+	toimpl()
+}
+
+/** Reset restores the paint to its initial settings. */
+func Reset() {
+	toimpl()
+}
+
+/** PaintHinting specifies the level of hinting to be performed. These names are
+taken from the Gnome/Cairo names for the same. They are translated into
+Freetype concepts the same as in cairo-ft-font.c:
+KPaintHintingNo     -> FT_LOAD_NO_HINTING
+KPaintHintingSlight -> FT_LOAD_TARGET_LIGHT
+KPaintHintingNormal -> <default, no option>
+KPaintHintingFull   -> <same as KPaintHintingNormal, unelss we are rendering
+					   subpixel glyphs, in which case TARGET_LCD or
+					   TARGET_LCD_V is used> */
 type PaintHinting int
 
 const (
 	KPaintHintingNo     = 0
 	KPaintHintingSlight = 1
-	KPaintHintingNormal = 2 // this is the default.
+	KPaintHintingNormal = 2 //< this is the default.
 	KPaintHintingFull   = 3
 )
 
@@ -46,6 +80,73 @@ func (paint *Paint) Hinting() PaintHinting {
 
 func (paint *Paint) SetHinting(hinting PaintHinting) {
 	paint.hinting = uint8(hinting)
+}
+
+/** Specifies the bit values that are stored in the paint's flags. */
+type PaintFlags int
+
+const (
+	KPaintFlagAntiAlias          PaintFlags = 0x01  //< mask to enable antialiasing
+	KPaintFlagDither             PaintFlags = 0x04  //< mask to enable dithering
+	KPaintFlagUnderline          PaintFlags = 0x08  //< mask to enable underline text
+	KPaintFlagStrikeThruText     PaintFlags = 0x10  //< mask to enable strike-thru text
+	KPaintFlagFakeBoldText       PaintFlags = 0x20  //< mask to enable fake-bold text
+	KPaintFlagLinearText         PaintFlags = 0x40  //< mask to enable linear-text
+	KPaintFlagSubpixelText       PaintFlags = 0x80  //< mask to enable subpixel text positioning
+	KPaintFlagDevKernText        PaintFlags = 0x100 //< mask to enable device kerning text
+	KPaintFlagLCDRenderText      PaintFlags = 0x200 //< mask to enable subpixel glyph renderering
+	KPaintFlagEmbeddedBitmapText PaintFlags = 0x400 //< mask to enable embedded bitmap strikes
+	KPaintFlagAutoHinting        PaintFlags = 0x800 //< mask to force Freetype's autohinter
+	KPaintFlagVerticalText       PaintFlags = 0x1000
+
+	/** hack for GDI -- do not use if you can help it
+	when adding extra flags, note that the fFlags member is specified
+	with a bit-width and you'll have to expand it. */
+	KPaintFlagGenA8FromLCD PaintFlags = 0x2000
+
+	KPaintFlagAllFlags PaintFlags = 0xFFFF
+)
+
+/** Return the paint's flags. Use the Flag enum to test flag values.
+@return the paint's flags (see enums ending in _Flag for bit masks) */
+func (paint *Paint) Flags() PaintFlags {
+	return PaintFlags(paint.flags)
+}
+
+/** Set the paint's flags. Use the Flag enum to specific flag values.
+@param flags    The new flag bits for the paint (see Flags enum) */
+func (paint *Paint) SetFlags(flags PaintFlags) {
+	paint.flags = uint16(flags)
+}
+
+/** Helper for getFlags(), returning true if kAntiAlias_Flag bit is set
+@return true if the antialias bit is set in the paint's flags. */
+func (paint *Paint) IsAntiAlias() bool {
+	return paint.Flags() & KPaintFlagAntiAlias != 0
+}
+
+/** Helper for setFlags(), setting or clearing the kAntiAlias_Flag bit
+@param aa   true to enable antialiasing, false to disable it */
+func (paint *Paint) SetAnitAlias(aa bool) {
+	toimpl()
+}
+
+/** Helper for getFlags(), returning true if kDither_Flag bit is set
+@return true if the dithering bit is set in the paint's flags. */
+func (paint *Paint) IsDither() bool {
+	return paint.Flags() & KPaintFlagDither != 0
+}
+
+/** Helper for setFlags(), setting or clearing the kDither_Flag bit
+@param dither   true to enable dithering, false to disable it */
+func (paint *Paint) SetDither(dither bool) {
+	toimpl()
+}
+
+/** Helper for getFlags(), returning true if kLinearText_Flag bit is set
+@return true if the lineartext bit is set in the paint's flags */
+func (paint *Paint) IsLinearText() bool {
+	return paint.Flags() & KPaintFlagLinearText != 0
 }
 
 func (paint *Paint) Looper() *DrawLooper {
@@ -70,30 +171,6 @@ func (paint *Paint) SetXfermodeMode(mode XfermodeMode) *Xfermode {
 	return paint.xfermode
 }
 
-type PaintFlags int
-
-const (
-	KPaintFlagAntiAlias          PaintFlags = 0x01
-	KPaintFlagDither             PaintFlags = 0x04
-	KPaintFlagUnderline          PaintFlags = 0x08
-	KPaintFlagStrikeThruText     PaintFlags = 0x10
-	KPaintFlagFakeBoldText       PaintFlags = 0x20
-	KPaintFlagLinearText         PaintFlags = 0x40
-	KPaintFlagSubpixelText       PaintFlags = 0x80
-	KPaintFlagDevKernText        PaintFlags = 0x100
-	KPaintFlagLCDRenderText      PaintFlags = 0x200
-	KPaintFlagEmbeddedBitmapText PaintFlags = 0x400
-	KPaintFlagAutoHinting        PaintFlags = 0x800
-	KPaintFlagVerticalText       PaintFlags = 0x1000
-
-	// hack for GDI -- do not use if you can help it when adding extra flags,
-	// note that the flags member is specified with a bit-width and you'll have
-	// expand it.
-	KPaintFlagGenA8FromLCD PaintFlags = 0x2000
-
-	KPaintFlagAllFlags PaintFlags = 0xFFFF
-)
-
 type PaintStyle int
 
 const (
@@ -101,14 +178,6 @@ const (
 	KPaintStyleStroke                           // < stroke the geometry
 	KPaintStyleStrokeAndFill                    // < fill and stroke the geometry
 )
-
-func (paint *Paint) Flags() PaintFlags {
-	return PaintFlags(paint.flags)
-}
-
-func (paint *Paint) SetFlags(flags PaintFlags) {
-	paint.flags = uint16(flags)
-}
 
 func (paint *Paint) CanComputeFastBounds() bool {
 	// if paint.Looper() != nil {
---
+++ write_buffer.go
@@ -0,0 +1,4 @@
+package ggk
+
+type WriteBuffer struct {
+}
